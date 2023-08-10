74a5f9b006c0e503c27b5f83db292502d07bb76c
ahmedfci2018@gmail.com <ahmedfci2018@gmail.com>
2023-08-10 11:04:53 +0300

    add script to code

--- platform/themes/hously/partials/header.blade.php
+++ platform/themes/hously/partials/header.blade.php
@@ -29,6 +29,14 @@
         })(window,document,'script','dataLayer','GTM-K6FMSWK');</script>
         <!-- End Google Tag Manager -->
 
+        <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KC68D9H3');</script>
+    <!-- End Google Tag Manager -->
+
         {!! Theme::header() !!}
     </head>
 
@@ -37,6 +45,12 @@
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6FMSWK"
             height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <!-- End Google Tag Manager (noscript) -->
+
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KC68D9H3"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+
         {!! apply_filters(THEME_FRONT_BODY, null) !!}
 
         <div id="alert-container"></div>
