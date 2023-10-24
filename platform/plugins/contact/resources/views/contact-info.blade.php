28eae49d4fd6810f993cef0a2dd667d6802d7558
unknown <ahmedfci2018>
2023-10-24 12:44:04 +0300

    contact form

--- platform/plugins/contact/resources/views/contact-info.blade.php
+++ platform/plugins/contact/resources/views/contact-info.blade.php
@@ -4,6 +4,11 @@
     <p>{{ trans('plugins/contact::contact.tables.email') }}: <i><a href="mailto:{{ $contact->email }}">{{ $contact->email }}</a></i></p>
     <p>{{ trans('plugins/contact::contact.tables.phone') }}: <i>@if ($contact->phone) <a href="tel:{{ $contact->phone }}">{{ $contact->phone }}</a> @else N/A @endif</i></p>
     <p>{{ trans('plugins/contact::contact.tables.address') }}: <i>{{ $contact->address ?: 'N/A' }}</i></p>
+    <p>{{ __('Unit Type') }}: <i>{{ $contact->unit_type ?: 'N/A' }}</i></p>
+    <p>{{ __('Purpose Of Purchase') }}: <i>{{ $contact->purpose_of_purchase ?: 'N/A' }}</i></p>
+    <p>{{ __('Purchase Place') }}: <i>{{ $contact->purchase_place ?: 'N/A' }}</i></p>
+    <p>{{ __('square') }}: <i>{{ $contact->square ?: 'N/A' }} M<sup>2</sup></i></p>
+    <p>{{ __('budget') }}: <i>{{ $contact->budget ?: 'N/A' }}</i></p>
     <p>{{ trans('plugins/contact::contact.tables.subject') }}: <i>{{ $contact->subject ?: 'N/A' }}</i></p>
     <p>{{ trans('plugins/contact::contact.tables.content') }}:</p>
     <pre class="message-content">{{ $contact->content ?: '...' }}</pre>
