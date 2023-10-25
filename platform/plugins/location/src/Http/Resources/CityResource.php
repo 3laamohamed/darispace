d5e23a210905757938017bf085c317f8c30a6a4f
unknown <ahmedfci2018>
2023-10-25 23:24:28 +0300

    content optional

--- platform/plugins/location/src/Http/Resources/CityResource.php
+++ platform/plugins/location/src/Http/Resources/CityResource.php
@@ -11,6 +11,7 @@ class CityResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'state_id' => $this->state_id,
         ];
     }
 }
